33a74c539531bcc731f71e72266ee1fdd2343b8a
atrakeur <atrakeur@gmail.com>
2013-10-05 13:21:30 +0200

    Removed ivy dependency management

---
+++ src/engine/physics/Physics.java
@@ -0,0 +1,13 @@
+package engine.physics;
+
+import engine.math.Vec2;
+
+public class Physics {
+	
+	public Physics()
+	{
+		Vec2 gravity = new Vec2(0.0f, 0.0f);
+		//World world = new World(gravity, true);
+	}
+
+}
